1584ea7bcc41f6177d6f5c043e3b6badc73036c1
Лев Будко <l.budko@team.superjob.ru>
2024-04-22 12:54:31 +0400

    пробелы

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 const API_BASE_URL = "http://localhost:8080";
-console.log(API_BASE_URL)
+console.log(API_BASE_URL) //fsdf
--- logout.js
+++ logout.js
@@ -1,6 +1,6 @@
 function logout() {
     // Удаление токена из хранилища
     localStorage.removeItem('auth_token');
-    // Переадресация на страницу входа или на главную страницу
+    // Переадресация на страницу входа или на главasdfную страницу
     window.location.href = '/login.html';
 }
